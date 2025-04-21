438d4623f6243a792daf1a9103bdd090f3dfe547
Elisha Abriham <116567028+Elish-Ab@users.noreply.github.com>
2025-04-21 20:47:32 +0300

    Update FormContent.tsx

--- src/app/components/FormContent.tsx
+++ src/app/components/FormContent.tsx
@@ -69,7 +69,7 @@ export default function FormContent() {
 
   return (
     // This is the main form component
-    <div className="min-h-screen bg-[#FAF8FF] py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-[#FAF8FF] py-8 px-4 sm:px-6 lg:px-8">
       <div className="max-w-3xl mx-auto">
         <h1 className="text-4xl font-bold text-center mb-8">
         Search for an  Information
